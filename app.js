00d53a9c1000fbbf755b2ec6b910c73b66cb593e
yandiaan <diansetiawan2121@gmail.com>
2023-07-18 06:01:31 +0700

    final gak kelar kelar cuy

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT
 
 //middlewares
 app.use(express.json())
-app.use(cors())
+app.use(cors({ origin: 'http://localhost:8100' }));
 
 //routes
 readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)))
