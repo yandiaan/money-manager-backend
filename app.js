2350565b52b0a4890270d2493b8da73ebf6dd1a7
yandiaan <diansetiawan2121@gmail.com>
2023-07-18 05:41:11 +0700

    final cuyy

--- app.js
+++ app.js
@@ -1,38 +1,40 @@
-const express = require('express')
-const cors = require('cors');
-const { connectToDB } = require('./db/db');
-const {readdirSync} = require('fs')
-const app = express()
-const authRoutes = require('./routes/auth');
-const userRoutes = require('./routes/user');
+const express = require("express");
+const cors = require("cors");
+const { connectToDB } = require("./db/db");
+const { readdirSync } = require("fs");
+const app = express();
+const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
 
-require('dotenv').config()
+require("dotenv").config();
 
-const PORT = process.env.PORT
-
-var corsOptions = {
-    origin: 'http://localhost:8100',
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-  }
+const PORT = process.env.PORT;
 
 //middlewares
-app.use(express.json())
-app.use(cors(corsOptions))
+app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 //routes
-readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)))
-app.use('/api/v1/auth', authRoutes);
-app.use('/api/v1/user', userRoutes);
-
+readdirSync("./routes").map((route) =>
+  app.use("/api/v1", require("./routes/" + route))
+);
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/user", userRoutes);
 
 const server = () => {
-    connectToDB().then(()=> {
-        app.listen(PORT, () => {
-            console.log('listening to port:', PORT)
-        })
-    }).catch((err)=> {
-        console.error("Error Connect : ",err)
+  connectToDB()
+    .then(() => {
+      app.listen(PORT, () => {
+        console.log("listening to port:", PORT);
+      });
     })
-}
+    .catch((err) => {
+      console.error("Error Connect : ", err);
+    });
+};
 
-server()
+server();
